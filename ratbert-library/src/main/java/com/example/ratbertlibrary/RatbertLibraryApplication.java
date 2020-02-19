b114e7077ba0a41487954fc53f8afb6658a9073f
Giorgio Bar <giorgio.bar@gmail.com>
2020-02-19 16:02:52 +0100

    Service for simple Hello World message

---
+++ ratbert-library/src/main/java/com/example/ratbertlibrary/RatbertLibraryApplication.java
@@ -0,0 +1,13 @@
+package com.example.ratbertlibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RatbertLibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RatbertLibraryApplication.class, args);
+	}
+
+}
---
+++ ratbert-library/src/main/java/com/example/ratbertlibrary/service/MyService.java
@@ -0,0 +1,11 @@
+package com.example.ratbertlibrary.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class MyService {
+
+    public String message() {
+        return "Hello, World!";
+    }
+}
